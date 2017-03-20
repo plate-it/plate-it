34632128fa244841d83d5283bc4fe3eaa6cc3658
bobby <bwuphan@gmail.com>
2017-03-20 12:19:57 -0700

    Add mongoose to server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('port', (process.env.PORT || 3001));
+
+const uri = 'mongodb://localhost/plateit';
+
+mongoose.connect(uri);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to Mongoose');
+});
 
 app.get('/test', (req, res) => {
   res.send('hello world');
