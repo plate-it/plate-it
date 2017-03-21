5d266b49959accae0701e69f5960277b9e6a0d8b
bobby <bwuphan@gmail.com>
2017-03-21 10:04:57 -0700

    Add getOne user method to userController

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const userController = require('./users/userController.js');
 const recipeController = require('./recipes/recipeController.js');
-const bodyParser = require('body-parser');
+
 
 module.exports = (app) => {
   app.post('/api/signup', userController.signup);
