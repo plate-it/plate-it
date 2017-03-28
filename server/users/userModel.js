1e7b3d6bb1051bda9b35a80bf318a04de412e4b2
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-28 07:46:37 -0700

    Add basic tests for user model

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String },
+  username: { type: String, required: true },
   books: [new Schema({
     bookname: String,
     recipeIds: [Number],
