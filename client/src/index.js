ab97e9ba0838e4c64537be46f39bd908531c3d13
Alex Autem <autem.alex@gmail.com>
2017-04-04 14:14:37 -0400

    Import react-router and routes.js to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {browserHistory} from 'react-router';
+import makeRoutes from './views/Main/routes';
+
+const routes = makeRoutes();
 
 ReactDOM.render(
-  <App />,
+  <App history={browserHistory} routes={routes} />,
   document.getElementById('root')
 );
