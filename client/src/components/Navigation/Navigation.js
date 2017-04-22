b09d2490a346eb570ba0256df25761036b3cde08
bobby <bwuphan@gmail.com>
2017-04-21 17:08:10 -0700

    Work in progress Navigation

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
 import Auth from '../Auth/Auth.js';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default class Navigation extends Component {
   constructor(props) {
@@ -13,13 +14,38 @@ export default class Navigation extends Component {
 
   render () {
     return (
-      <div>
-        <IndexLink to='/' style={{padding: 10}}>Landing</IndexLink>
-        <Auth />
-        <Link to='/recipe' style={{padding: 10}}>Create Recipe</Link>
-        <Link to='/collections' style={{padding: 10}}>View Collections</Link>
-      </div>
+     <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">Plate It</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1}><Link to="/recipe">Recipes</Link></NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1} >Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
     )
   }
 
 }
+// <div>
+//   <IndexLink to='/' style={{padding: 10}}>Landing</IndexLink>
+//   <Auth />
+//   <Link to='/recipe' style={{padding: 10}}>Create Recipe</Link>
+//   <Link to='/collections' style={{padding: 10}}>View Collections</Link>
+// </div>
