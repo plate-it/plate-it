aa6e7ce3cc85fb32834c0a8efd7f450717cf7b2c
bobby <bwuphan@gmail.com>
2017-04-25 22:22:12 -0700

    Add active links for Navigation

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import Auth from '../Auth/Auth.js';
 import { Menu, Segment } from 'semantic-ui-react';
 
-export default class Navigation extends Component {
-
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+  }
   render () {
+    const { location } = this.props;
     return (
      <Segment color="blue" inverted>
-      <Menu color="blue" inverted borderless>
+      <Menu color="blue" inverted >
         <Menu.Item
           as={Link}
           to="/"
+          active={location ==='/'}
         >
           Plate-It
         </Menu.Item>
-        <Menu.Item as={Link} to='/recipe' color="green" name="Create Recipe" />
-        <Menu.Item as={Link} to='/collections' color="green" name="View Collections" />
+        <Menu.Item as={Link} to='/recipe' color="green" name="Create Recipe" active={location ==='/recipe'}/>
+        <Menu.Item as={Link} to='/collections' color="green" name="View Collections" active={location ==='/collections'} />
       </Menu>
     </Segment>
-    )
+    );
   }
-
 }
+
+const mapStateToProps = (state) => {
+  let location = '';
+  if (state.routing.locationBeforeTransitions !== null) {
+    location = state.routing.locationBeforeTransitions.pathname;
+  }
+  return { location };
+};
+
+export default connect(mapStateToProps)(Navigation);
+
 // <div>
 //   <IndexLink to='/' style={{padding: 10}}>Landing</IndexLink>
 //   <Auth />
