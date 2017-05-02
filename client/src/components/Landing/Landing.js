61808b623e5809118014806fb893b0a1bebed0a9
maxuuell <maxuuell@gmail.com>
2017-05-02 16:12:16 -0700

    Add feature section and banner to landing page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -12,8 +12,45 @@ export default class Landing extends Component {
 
   render () {
     return (
-      <div>
-        <p>Check out our amazing app!</p>
+      <div
+        className='wrapper'
+      >
+        <section
+          className='landing-banner container'
+        >
+          <h2
+            className='landing-header'
+          >The last cook book you'll ever need</h2>
+          <h3
+            className='landing-description'
+          >Create and manage all of your recipes in one place.</h3>
+        </section>
+        <section 
+          className='landing-features'
+        >
+          <article
+            className='feature container'
+          >
+            <p
+              className='feature-text'
+            >Create uniform formatted recipes with the inline text editor.</p>
+          </article>
+          <article
+            className='feature container'
+          >
+            <p
+              className='feature-text'
+            >Organize your recipes in books that can be given titles to reflect the theme of the collection.</p>
+          </article>
+          <article
+            className='feature container'
+          >
+            <p
+              className='feature-text'
+            >Share your recipes with friends and families on social media, or with sharable links.</p>
+          </article>
+        </section>
+
       </div>
     )
   }
